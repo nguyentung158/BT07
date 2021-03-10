d1be74f0cc7b15dbabd9706b771d123eb7331008
nguyentung158 <78079771+nguyentung158@users.noreply.github.com>
2021-03-10 16:15:23 +0700

    Add files via upload

---
+++ A2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void  ham(int a[]){
+          cout << sizeof(a) << endl;
+          }
+int main(){
+    int a[10];
+
+    ham(a);
+
+cout << sizeof(a) <<endl;
+
+}
